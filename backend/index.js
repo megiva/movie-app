7f78b52918a575bc8618ca20cfa07ee655cea224
megiva <meghana.m.ivaturi@gmail.com>
2025-09-09 22:43:28 -0400

    Fix: remove nested git repo and re-add backend correctly

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+dotenv.config();
+import ReviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient = mongodb.MongoClient
+const mongo_username = process.env['MONGO_USERNAME']
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.dbiv8tu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const port = 8000
+
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+    })
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        await ReviewsDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
